c73ab93a8bbd7da92e0d43c7e4539981d30b49f1
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-12 16:52:36 +0200

    Make radio button component

---
+++ src/components/RadioButton/RadioButton.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./RadioButton.css";
+import {InsuranceOption } from "../../utils/fetchMockData"; 
+
+interface RadioButtonProps extends Pick<InsuranceOption, "title" | "description" | "price" | "recommended" > {
+    selected: boolean;
+    onChange: () => void;
+  }
+
+const RadioButton: React.FC<RadioButtonProps> = ({
+  title,
+  description,
+  price,
+  selected,
+  recommended,
+  onChange,
+}) => {
+    const uniqueId = `radio-${title}`;
+    return (
+        <label className={`radio-button ${selected ? "selected" : ""} ${recommended ? "recommended" : ""}`}>
+            {recommended && <span className="recommended-badge">Anbefalt</span>}
+            <input 
+              type="radio" 
+              id={uniqueId} 
+              name="insurance-options"
+              checked={selected} 
+              value={title} 
+              onChange={onChange} 
+            />
+            <span className="checkmark" aria-hidden="true"></span>
+            <div className="radio-content">
+            <div className="radio-header">
+              <p>{title}</p>
+              <p>{price} kr/md.</p>
+            </div>
+            {selected && description && <p className="radio-description">{description}</p>}
+          </div>
+        </label>
+      );
+    };
+    
+
+export default RadioButton;
