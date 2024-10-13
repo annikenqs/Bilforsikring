371662ebc4f3fc73911a8e341689d82fcb51e101
Anniken Strand <annikeqs@stud.ntnu.no>
2024-10-13 13:06:01 +0200

    Update radiobutton component

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./RadioButton.css";
-import {InsuranceOption } from "../../utils/fetchMockData"; 
+import { InsuranceOption } from "../../utils/fetchMockData";
 
-interface RadioButtonProps extends Pick<InsuranceOption, "title" | "description" | "price" | "recommended" > {
-    selected: boolean;
-    onChange: () => void;
-  }
+interface RadioButtonProps
+  extends Pick<
+    InsuranceOption,
+    "title" | "description" | "price" | "recommended"
+  > {
+  selected: boolean;
+  onChange: () => void;
+}
 
 const RadioButton: React.FC<RadioButtonProps> = ({
   title,
@@ -15,29 +19,34 @@ const RadioButton: React.FC<RadioButtonProps> = ({
   recommended,
   onChange,
 }) => {
-    const uniqueId = `radio-${title}`;
-    return (
-        <label className={`radio-button ${selected ? "selected" : ""} ${recommended ? "recommended" : ""}`}>
-            {recommended && <span className="recommended-badge">Anbefalt</span>}
-            <input 
-              type="radio" 
-              id={uniqueId} 
-              name="insurance-options"
-              checked={selected} 
-              value={title} 
-              onChange={onChange} 
-            />
-            <span className="checkmark" aria-hidden="true"></span>
-            <div className="radio-content">
-            <div className="radio-header">
-              <p>{title}</p>
-              <p>{price} kr/md.</p>
-            </div>
-            {selected && description && <p className="radio-description">{description}</p>}
-          </div>
-        </label>
-      );
-    };
-    
+  const uniqueId = `radio-${title}`;
+  return (
+    <label
+      className={`radio-button ${selected ? "selected" : ""} ${
+        recommended ? "recommended" : ""
+      }`}
+    >
+      {recommended && <span className="recommended-badge">Anbefalt</span>}
+      <input
+        type="radio"
+        id={uniqueId}
+        name="insurance-options"
+        checked={selected}
+        value={title}
+        onChange={onChange}
+      />
+      <span className="checkmark" aria-hidden="true"></span>
+      <div className="radio-content">
+        <div className="radio-header">
+          <h4>{title}</h4>
+          <p>{price} kr/md.</p>
+        </div>
+        {selected && description && (
+          <p className="radio-description">{description}</p>
+        )}
+      </div>
+    </label>
+  );
+};
 
 export default RadioButton;
